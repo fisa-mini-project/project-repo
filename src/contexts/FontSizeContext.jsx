f74cd845ca8083ec164cff36f6237212a725c3fe
CHICHIT <beflylee@naver.com>
2025-07-16 10:05:46 +0900

    fontsize context화

---
+++ src/contexts/FontSizeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from 'react';
+
+
+export const FontSizeContext = createContext();
+
+export const FontSizeProvider = ({ children }) => {
+  const [fontSizeLevel, setFontSizeLevel] = useState('medium');
+
+  return (
+    <FontSizeContext.Provider value={{ fontSizeLevel, setFontSizeLevel }}>
+      {children}
+    </FontSizeContext.Provider>
+  );
+};
+
+// 쉽게 사용하도록 커스텀 훅 제공
+export const useFontSize = () => useContext(FontSizeContext);
